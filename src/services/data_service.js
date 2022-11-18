41c380dc2308dfea33e55e0ff691d51c7e91be2b
wazadio <junidio306@gmail.com>
2022-11-18 14:58:55 +0700

    replace system detail data to server's while unable to fetch from client request

--- src/services/data_service.js
+++ src/services/data_service.js
@@ -1,6 +1,6 @@
-const DataModel = require("../models/data_model")
 const DeviceDetector = require("device-detector-js")
-
+const os = require("os")
+const DataModel = require("../models/data_model")
 
 class DataService {
     constructor() {
@@ -10,17 +10,17 @@ class DataService {
     async createData(req, res) {
         try {
             const clientDetail = this.deviceDetector.parse(req.header("user-agent"))
-            let [remainingRamValue, totalRamValue] = [0, 0]
+            let [remainingRamValue, totalRamValue] = [os.freemem(), os.totalmem()]
             if (req.header("device-memory")) {
                 remainingRamValue = Number(req.header("device-memory"))
                 totalRamValue = Number(req.header("device-memory"))
             }
 
             const newData = {
-                cpuname: clientDetail.device.model,
-                type: clientDetail.os.name,
-                platform: clientDetail.os.platform,
-                release: clientDetail.os.version,
+                cpuname: clientDetail.device.model || os.cpus()[0].model,
+                type: clientDetail.os.name || os.version(),
+                platform: clientDetail.os.platform || os.platform(),
+                release: clientDetail.os.version || os.release(),
                 remainingRam: remainingRamValue,
                 totalRam: totalRamValue
             }
